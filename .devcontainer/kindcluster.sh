278a2b053ca05ebd71a6d60b93dea211a7cb7a09
VSSS-start <vamshisakeths@gmail.com>
2025-02-09 16:42:10 +0530

    Update kindcluster.sh

--- .devcontainer/kindcluster.sh
+++ .devcontainer/kindcluster.sh
@@ -9,3 +9,17 @@ nodes:
 
 
 kind create cluster --image=kindest/node:v1.30.8@sha256:17cd608b3971338d9180b00776cb766c50d0a0b6b904ab4ff52fd3fc5c6369bf --name cka-cluster1 --config config.yml
+
+
+
+cat > config.yml    # specific to kind only
+# three node (two workers) cluster config
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+  extraPortMappings:
+  - containerPort: 80
+    hostPort: 80
+- role: worker
+- role: worker
