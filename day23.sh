a2ca872f36d674e0967918060fa96817f392502f
VSSS-start <vamshisakeths@gmail.com>
2025-02-11 18:39:31 +0530

    Create day23.sh

---
+++ day23.sh
@@ -0,0 +1,13 @@
+1  cd $HOME
+    2  ls
+    3  ls -ltr
+    4  cd /Users/
+    5  cd /workspaces/Test-Kubernetes/
+    6  $HOME
+    7  cd /home
+    8  ls -ltr
+    9  ls -a
+   10  cd /workspaces/Test-Kubernetes/
+   11  docker ps 
+   12  docker exec -it cka-cluster1-control-plane sh
+   13  history > day23
