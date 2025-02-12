1d3aa3c6e85e65544ebc903cdf82fdc18353bbd0
VSSS-start <vamshisakeths@gmail.com>
2025-02-13 02:16:58 +0530

    Create day29.sh

---
+++ day29.sh
@@ -0,0 +1,70 @@
+cd ../..; mkdir day29;cd day29
+   80  docker ps -a
+   81  docker rm -f 3b79981f89f6
+   82  docker ps -a
+   83  cat > VolumeMountPod.yaml
+   84  kubectl apply -f VolumeMountPod.yaml 
+   85  cat > VolumeMountPod.yaml
+   86  kubectl apply -f VolumeMountPod.yaml 
+   87  cat > VolumeMountPod.yaml
+   88  kubectl apply -f VolumeMountPod.yaml 
+   89  cat > VolumeMountPod.yaml
+   90  kubectl apply -f VolumeMountPod.yaml 
+   91  cat > VolumeMountPod.yaml
+   92  kubectl apply -f VolumeMountPod.yaml 
+   93  mkdir -p data/redis
+   94  ls
+   95  kubectl apply -f VolumeMountPod.yaml 
+   96  cat > VolumeMountPod.yaml
+   97  kubectl apply -f VolumeMountPod.yaml 
+   98  cat > VolumeMountPod.yaml
+   99  kubectl apply -f VolumeMountPod.yaml 
+  100  kubectl get po 
+  101* 
+  102  cd 
+  103* 
+  104  ls -a
+  105  ls -ltr
+  106  kubectl exec -it redis -- sh
+  107  kubectl delete po redis 
+  108  kubectl exec -it redis -- sh
+  109  kubectl apply -f VolumeMountPod.yaml 
+  110  cd /workspaces/Test-Kubernetes/day29/
+  111  ls
+  112  kubectl apply -f VolumeMountPod.yaml 
+  113  kubectl exec -it redis -- sh
+  114  cat > PersistentVolume.yaml
+  115  kubectl apply -f PersistentVolume.yaml 
+  116  cat > PersistentVolumeClaim.yaml
+  117  kubectl apply -f PersistentVolumeClaim.yaml 
+  118  kubectl get pv
+  119  kubectl get pvc
+  120  kubectl describe  pvc task-pv-claim
+  121  cat > PersistentVolumePod.yaml
+  122  kubectl apply -f PersistentVolumePod.yaml 
+  123  cat > PersistentVolumePod.yaml
+  124  kubectl apply -f PersistentVolumePod.yaml 
+  125  cat > PersistentVolumePod.yaml
+  126  kubectl apply -f PersistentVolumePod.yaml 
+  127  cat > PersistentVolumePod.yaml
+  128  kubectl apply -f PersistentVolumePod.yaml 
+  129  cat > PersistentVolumePod.yaml
+  130  kubectl apply -f PersistentVolumePod.yaml 
+  131  cat > PersistentVolumePod.yaml
+  132  kubectl apply -f PersistentVolumePod.yaml 
+  133  cat > PersistentVolumePod.yaml
+  134  kubectl apply -f PersistentVolumePod.yaml 
+  135  cat > PersistentVolumePod.yaml
+  136  kubectl delete pod/task-pv-pod
+  137  kubectl apply -f PersistentVolumePod.yaml 
+  138  kubectl get nodes
+  139  cat > PersistentVolumePod.yaml
+  140  kubectl apply -f PersistentVolumePod.yaml 
+  141  cat > PersistentVolumePod.yaml
+  142  kubectl apply -f PersistentVolumePod.yaml 
+  143  kubectl get pvc
+  144  kubectl exec -it task-pv-pod -- bash
+  145  cat > PersistentVolumePod.yaml
+  146  kubectl delete pod/task-pv-pod
+  147  kubectl apply -f PersistentVolumePod.yaml 
+  148  kubectl exec -it task-pv-pod -- bash
